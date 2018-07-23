56023931c36ea05df16ca2d485bc1a89174e7dfd
mbow1 <mbow.morniass@gmail.com>
2018-07-23 12:35:33 +0200

    Creation du fichier deploy

--- Script/deploy.sh
+++ Script/deploy.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+
+
+if [! -e BDD_Intermédiaire.sql]
+then {
+    echo "Le fichier BDD_Intermediaire est introuvable !!";
+    exit 1;
+}
+if [! -e BDD_Intermédiaire.sql]
+then {
+    echo "Le fichier BDD_Intermediaire est introuvable !!";
+    exit 1;
+}
+
+
 sudo apt-get update;
 sudo apt-get install python -y;
 sudo apt-get install mysql-server -y;
@@ -21,3 +35,29 @@ crontab mycron
 rm mycron
 
 #echo "SELECT * FROM Locales" | mysql --user=test BDD_INTER
+
+# create random password
+#PASSWDDB="$(openssl rand -base64 12)"
+PASSWDDB="";
+
+# replace "-" with "_" for database username
+MAINDB="TwindevUser"
+
+# If /root/.my.cnf exists then it won't ask for root password
+if [ -f /root/.my.cnf ]; then
+
+    mysql -e "CREATE USER ${MAINDB}@localhost IDENTIFIED BY '${PASSWDDB}';"
+    mysql -e "GRANT ALL PRIVILEGES ON ${MAINDB}.* TO '${MAINDB}'@'localhost';"
+    mysql -e "FLUSH PRIVILEGES;"
+
+# If /root/.my.cnf doesn't exist then it'll ask for root password   
+else
+    echo "Please enter root user MySQL password!"
+    read rootpasswd
+    mysql -uroot -p${rootpasswd} -e "CREATE USER ${MAINDB}@localhost IDENTIFIED BY '${PASSWDDB}';"
+    mysql -uroot -p${rootpasswd} -e "GRANT ALL PRIVILEGES ON ${MAINDB}.* TO '${MAINDB}'@'localhost';"
+    mysql -uroot -p${rootpasswd} -e "FLUSH PRIVILEGES;"
+fi
+
+mysql -u ${MAINDB} -p  ${MAINDB} < db.sql
+
